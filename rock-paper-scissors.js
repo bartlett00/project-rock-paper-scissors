a6979c975be6161b71f6d80ff5cd315d6fbc3fbc
Pax <113464089+bartlett00@users.noreply.github.com>
2024-04-16 10:51:28 -0500

    Write function that plays round of game.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,50 @@
 function getComputerChoice() {
-    
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    if (randomNum == 1) {
+        return 'rock';
+    } else if (randomNum == 2) {
+        return 'paper';
+    } else if (randomNum == 3) {
+        return 'scissors';
+    }
 }
+console.log(getComputerChoice());
+
+playRound(playerSelection, computerSelection) {
+   let playerChoice = playerSelection.toLowerCase();
+   let winOrLose = false;
+
+// algo. that determines who wins 
+
+   if (playerChoice == computerSelection) {
+    return 'It is a tie!';
+   } else if (playerChoice == 'rock') {
+        if(computerSelection == 'paper') {
+            winOrLose = false;
+        } else {
+            winOrLose = true;
+        }
+   } else if (playerChoice == 'paper') {
+        if (computerSelection == 'rock') {
+            winOrLose = true;
+        } else {
+            winOrLose = false;
+        }
+   } else if (playerChoice == 'scissors') {
+        if (computerSelection == 'paper') {
+            winOrLose = true;
+        } else {
+            winOrLose = false;
+        }
+   }
+
+// Checks if player wins or looses and returns appropriate message
+
+   if (winOrLose == true) {
+        return `You win! ${playerChoice} beats ${computerSelection}`;        
+   } else {
+        return `You lose! ${computerSelection} beats ${playerChoice}`;
+   }
+}
+
